c8a7381a46efcd8d1f08c5608065786681128775
Brian Illes <brianrilles@gmail.com>
2019-02-06 08:07:59 -0800

    small tweaks

---
+++ Components/Navigation/navigation.js
@@ -0,0 +1,17 @@
+// class Menu {
+//   constructor(element) {
+//     this.element = element;
+//     this.button = this.element.querySelector(".drop");
+//     this.content = this.element.querySelector(".nav-show");
+//     this.button.addEventListener("click", () => this.toggleMenu());
+//   }
+//   toggleMenu() {
+//     this.content.classList.toggle(".show");
+//   }
+// }
+
+// let menus = document
+//   .querySelectorAll(".header-container")
+//   .forEach(menu => new Menu(menu));
+
+// console.log(menus);
