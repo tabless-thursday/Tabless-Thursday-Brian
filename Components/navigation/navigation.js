339af728a1facc78c8a42a15baea1d43992a46b3
Brian Illes <brianrilles@gmail.com>
2019-02-07 09:07:36 -0800

    minor tweaks

--- Components/navigation/navigation.js
+++ Components/navigation/navigation.js
@@ -1,21 +1,11 @@
-const toggleMenu = () => {
-  menu.classList.toggle("menu");
-};
+// const toggleMenu = () => {
+//   menu.classList.toggle("menu");
+// };
 
-const menu = document.querySelector("nav");
+// const menu = document.querySelector("nav");
 
-const menuBtn = document.querySelector("nav i");
+// const menuBtn = document.querySelector("nav i");
 
-menuBtn.addEventListener("click", () => toggleMenu());
+// menuBtn.addEventListener("click", () => toggleMenu());
 
 
-//Ticker
-const ticker = document.getElementsByClassName("ticker");
-
-function getUsers() {
-    //res.json must be number
-    fetch('tbd')
-      .then(res => {
-          ticker.textContent = res.json()})
-      .catch(err => console.log(er));
-}
---
+++ users.js
@@ -0,0 +1,10 @@
+// //Ticker
+// const ticker = document.getElementsByClassName("ticker");
+
+// function getUsers() {
+//     //res.json must be number
+//     fetch('tbd')
+//       .then(res => {
+//           ticker.textContent = res.json()})
+//       .catch(err => console.log(er));
+// }
